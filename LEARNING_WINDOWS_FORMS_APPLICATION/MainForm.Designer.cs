55121bf10b997812748d292033d74af3d261dc66
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-02-23 13:14:19 +0330

    Add project files.

---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.fullNameTextBox = new System.Windows.Forms.TextBox();
+			this.fullNameLabel = new System.Windows.Forms.Label();
+			this.insertButton = new System.Windows.Forms.Button();
+			this.fullNameslistBox = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// fullNameTextBox
+			// 
+			this.fullNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.fullNameTextBox.Location = new System.Drawing.Point(72, 12);
+			this.fullNameTextBox.Name = "fullNameTextBox";
+			this.fullNameTextBox.Size = new System.Drawing.Size(204, 20);
+			this.fullNameTextBox.TabIndex = 1;
+			// 
+			// fullNameLabel
+			// 
+			this.fullNameLabel.AutoSize = true;
+			this.fullNameLabel.Location = new System.Drawing.Point(12, 15);
+			this.fullNameLabel.Name = "fullNameLabel";
+			this.fullNameLabel.Size = new System.Drawing.Size(54, 13);
+			this.fullNameLabel.TabIndex = 0;
+			this.fullNameLabel.Text = "&Full Name";
+			// 
+			// insertButton
+			// 
+			this.insertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.insertButton.Location = new System.Drawing.Point(282, 10);
+			this.insertButton.Name = "insertButton";
+			this.insertButton.Size = new System.Drawing.Size(29, 23);
+			this.insertButton.TabIndex = 2;
+			this.insertButton.Text = "...";
+			this.insertButton.UseVisualStyleBackColor = true;
+			this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+			// 
+			// fullNameslistBox
+			// 
+			this.fullNameslistBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.fullNameslistBox.FormattingEnabled = true;
+			this.fullNameslistBox.Location = new System.Drawing.Point(12, 38);
+			this.fullNameslistBox.Name = "fullNameslistBox";
+			this.fullNameslistBox.Size = new System.Drawing.Size(299, 290);
+			this.fullNameslistBox.TabIndex = 3;
+			this.fullNameslistBox.Enter += new System.EventHandler(this.fullNameslistBox_Enter);
+			this.fullNameslistBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.fullNameslistBox_KeyDown);
+			// 
+			// MainForm
+			// 
+			this.AcceptButton = this.insertButton;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(323, 336);
+			this.Controls.Add(this.fullNameslistBox);
+			this.Controls.Add(this.insertButton);
+			this.Controls.Add(this.fullNameLabel);
+			this.Controls.Add(this.fullNameTextBox);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Main";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox fullNameTextBox;
+		private System.Windows.Forms.Label fullNameLabel;
+		private System.Windows.Forms.Button insertButton;
+		private System.Windows.Forms.ListBox fullNameslistBox;
+	}
+}
+
---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
@@ -0,0 +1,127 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void insertButton_Click(object sender, System.EventArgs e)
+		{
+			// Solution (1)
+			//fullNameslistBox.Items.Add(fullNameTextBox.Text);
+			// /Solution (1)
+
+			// Solution (2)
+			//fullNameslistBox.Items.Add(fullNameTextBox.Text);
+
+			//fullNameTextBox.Text = string.Empty;
+
+			//fullNameTextBox.Focus();
+			// /Solution (2)
+
+			// Solution (3)
+			fullNameslistBox.Items.Add(fullNameTextBox.Text);
+
+			fullNameTextBox.Focus();
+
+			fullNameTextBox.SelectAll();
+
+			//fullNameTextBox.Select(start: 0, length: fullNameTextBox.Text.Length);
+
+			//fullNameTextBox.SelectionStart = 0;
+			//fullNameTextBox.SelectionLength = fullNameTextBox.Text.Length;
+			// /Solution (3)
+		}
+
+		private void fullNameslistBox_KeyDown
+			(object sender, System.Windows.Forms.KeyEventArgs e)
+		{
+			if ((e.Alt) || (e.Control) || (e.Shift))
+			{
+				return;
+			}
+
+			//// اگر کاربر دکمه دیلیت را زده بود
+			//if (e.KeyCode == System.Windows.Forms.Keys.Delete)
+			//{
+			//	// و اگر آیتمی انتخاب شده بود
+			//	if (fullNameslistBox.SelectedIndex >= 0)
+			//	{
+			//		// اندیس آیتم انتخاب شده را بدست آورده
+			//		// و از لیست باکس آنرا حذف می‌کنیم
+			//		fullNameslistBox.Items.RemoveAt(fullNameslistBox.SelectedIndex);
+			//	}
+			//}
+
+			//// اگر کاربر دکمه دیلیت را زده بود
+			//if (e.KeyCode == System.Windows.Forms.Keys.Delete)
+			//{
+			//	// و اگر آیتمی انتخاب شده بود
+			//	if (fullNameslistBox.SelectedIndex >= 0)
+			//	{
+			//		// اندیس آیتم انتخاب شده را بدست آورده
+			//		// و در متغیر کمکی ذخیره می‌کنیم
+			//		int intSelectedIndex =
+			//			fullNameslistBox.SelectedIndex;
+
+			//		// با توجه به اندیس انتخاب شده
+			//		// آیتمی با آن اندیس را لیست باکس حذف می‌کنیم
+			//		fullNameslistBox.Items.RemoveAt(fullNameslistBox.SelectedIndex);
+
+			//		// اندیس انتخاب شده لیست باکس را برابر اندیسی که قبلا
+			//		// انتخاب شده بود قرار می‌دهیم
+			//		// این بدان معنا است  که پس از حذف آیتم انتخاب شده
+			//		// دقیقا آیتم بعدی آن انتخاب می‌شود
+			//		fullNameslistBox.SelectedIndex = intSelectedIndex;
+			//	}
+			//}
+
+			// اگر کاربر دکمه دیلیت را زده بود
+			if (e.KeyCode == System.Windows.Forms.Keys.Delete)
+			{
+				// و اگر آیتمی انتخاب شده بود
+				if (fullNameslistBox.SelectedIndex >= 0)
+				{
+					// اندیس آیتم انتخاب شده را بدست آورده
+					// و در متغیر کمکی ذخیره می‌کنیم
+					int intSelectedIndex =
+						fullNameslistBox.SelectedIndex;
+
+					// با توجه به اندیس انتخاب شده
+					// آیتمی با آن اندیس را لیست باکس حذف می‌کنیم
+					fullNameslistBox.Items.RemoveAt(fullNameslistBox.SelectedIndex);
+
+					// اندیس انتخاب شده لیست باکس را برابر اندیسی که قبلا
+					// انتخاب شده بود قرار می‌دهیم
+					// این بدان معنا است  که پس از حذف آیتم انتخاب شده
+					// دقیقا آیتم بعدی آن انتخاب می‌شود
+
+					if (intSelectedIndex <= fullNameslistBox.Items.Count - 1)
+					{
+						fullNameslistBox.SelectedIndex = intSelectedIndex;
+					}
+					else
+					{
+						fullNameslistBox.SelectedIndex = fullNameslistBox.Items.Count - 1;
+					}
+				}
+			}
+		}
+
+		private void fullNameslistBox_Enter(object sender, System.EventArgs e)
+		{
+			// اگر لااقل یک آیتم در داخل لیست باکس وجود داشت
+			if (fullNameslistBox.Items.Count >= 1)
+			{
+				// و اگر هیچ آیتمی هنوز انتخاب نشده بود
+				if (fullNameslistBox.SelectedIndex == -1)
+				{
+					// می خواهیم اولین آیتم لیست باکس انتخاب شود
+					fullNameslistBox.SelectedIndex = 0;
+				}
+			}
+		}
+	}
+}
